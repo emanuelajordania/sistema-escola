e83edbe0f1d0a486d433d539630ae1eb18e10ea0
Emanuela Jordania <emanuelajord@gmail.com>
2024-01-18 17:20:25 -0300

    Inclusão das classes de testes e  alterações nas classes Aluno, Telefone,CPF

--- src/main/java/br/com/manu/escola/Email.java
+++ src/main/java/br/com/manu/escola/Email.java
@@ -6,10 +6,15 @@ public class Email {
 
     public Email(String endereco) {
         if (endereco == null ||
-                !endereco.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]{\\\\.[a-zA-Z]{2,}$")) {
+                !endereco.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
             throw new IllegalArgumentException("E-mail inválido!");
         }
 
         this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+
+        return endereco;
     }
 }
